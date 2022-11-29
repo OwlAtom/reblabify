c7ba8b2f430a2255ab142f7f3899bbf917bf0418
OwlAtom <57601828+OwlAtom@users.noreply.github.com>
2022-11-29 08:30:52 +0100

    3wnqyan > Feat(store): added Pinia with persisted and compressed state.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 
+const pinia = createPinia();
 const app = createApp(App);
 
+pinia.use(piniaPluginPersistedstate);
+
+app.use(pinia);
 app.use(router);
 app.mount("#app");
---
+++ src/stores/name.js
@@ -0,0 +1,40 @@
+import { ref, computed } from "vue";
+import { compressToUTF16, decompressFromUTF16 } from "lz-string";
+import { defineStore } from "pinia";
+
+export const useNameStore = defineStore(
+  "name",
+  () => {
+    const name = ref({
+      first: "Matt",
+      last: "Harrison",
+    });
+
+    const fullName = computed(() => {
+      return `${name.value.first} ${name.value.last}`;
+    });
+
+    const changeFirstName = (newName) => {
+      name.value.first = newName;
+    };
+    const changeLastName = (newName) => {
+      name.value.last = newName;
+    };
+    return {
+      name,
+      fullName,
+      changeFirstName,
+      changeLastName,
+    };
+  },
+  {
+    persist: {
+      serialize: (state) => {
+        return compressToUTF16(JSON.stringify(state));
+      },
+      deserialize: (state) => {
+        return JSON.parse(decompressFromUTF16(state));
+      },
+    },
+  }
+);
