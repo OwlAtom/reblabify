71eae2b36a7be399302477f97d3116a269557977
OwlAtom <57601828+OwlAtom@users.noreply.github.com>
2022-12-01 08:40:21 +0100

    3wnr712 > Feat: create normal button

---
+++ src/components/normalButton.vue
@@ -0,0 +1,45 @@
+<script setup>
+const props = defineProps({
+  text: String,
+  IconBefore: String,
+  IconAfter: String,
+});
+</script>
+
+<template>
+  <button ref="theButton" class="btn">
+    <span class="material-icons" v-if="IconBefore">{{ IconBefore }}</span>
+    {{ text }}
+    <span class="material-icons" v-if="IconAfter">{{ IconAfter }}</span>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+$darkTan: #edb183;
+
+.btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 0.5rem 1rem;
+  border-radius: 0.5em;
+  width: 100%;
+
+  font-family: "Maven Pro", sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 26px;
+  line-height: 31px;
+
+  background-color: $darkTan;
+  &.outlined {
+    background-color: transparent;
+    border: 2px solid $darkTan;
+    color: $darkTan;
+  }
+}
+.material-icons {
+  font-size: 1.5em;
+}
+</style>
