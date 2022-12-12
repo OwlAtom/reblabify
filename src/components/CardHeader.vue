855e7bd83cf3c3c06861be0719907db819ffdadc
KamillaKlavsen <kamillaklavsen@outlook.dk>
2022-12-12 21:56:18 +0100

    3wnra6g -> feat: oprettelse af komponent, og test på Landing

---
+++ src/components/CardHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  props: { title: String, icon: String },
+};
+</script>
+
+<template>
+  <div
+    class="flex flex-initial flex-row align-items-center justify-content-center column-gap-1"
+  >
+    <div class="material-icons" v-if="icon">{{ icon }}</div>
+    <h3>{{ title }}</h3>
+  </div>
+</template>
+
+<style scoped>
+.material-icons {
+  font-size: 1.5em;
+  margin: 0;
+  padding: 0;
+}
+</style>
