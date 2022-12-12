e5cd655a6aebfa2ee9630fa37841648b1019b7cb
OwlAtom <57601828+OwlAtom@users.noreply.github.com>
2022-12-12 14:39:27 +0100

    3xcpgwz > feat: configure eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,21 @@
 module.exports = {
+  root: true,
   env: {
+    browser: true,
+    es2021: true,
     node: true,
   },
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
   rules: {
     // todo: add rules
+    "vue/require-default-prop": 0,
+    "vue/html-indent": ["error", 4],
+    "vue/singleline-html-element-content-newline": 0,
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
   },
+  settings: {},
 };
