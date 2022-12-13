2c2ee0d2a77ac647a54eaa24ff2a8307fa47e212
OwlAtom <57601828+OwlAtom@users.noreply.github.com>
2022-12-13 08:44:22 +0100

    3xcpgwz > fix: have same indent in linter and formatter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,8 @@ module.exports = {
   rules: {
     // todo: add rules
     "vue/require-default-prop": 0,
-    "vue/html-indent": ["error", 4],
+    "indent": ["error", 2],
+    "vue/html-indent": ["error", 2],
     "vue/singleline-html-element-content-newline": 0,
     "vue/component-name-in-template-casing": ["error", "PascalCase"],
   },
