e3a0aff80454743b640676c3ce636f62c6735600
KamillaKlavsen <kamillaklavsen@outlook.dk>
2022-12-13 11:33:37 +0100

    Update CardHeader.vue
    padding og margin fjernet

--- src/components/CardHeader.vue
+++ src/components/CardHeader.vue
@@ -5,9 +5,9 @@ export default {
 </script>
 
 <template>
-  <div class="flex flex-grow-0 align-items-center column-gap-1 px-1">
+  <div class="flex flex-grow-0 align-items-center column-gap-1">
     <div v-if="icon" class="material-icons">{{ icon }}</div>
-    <h3 class="flex-grow-0">{{ title }}</h3>
+    <h3 class="flex-grow-0 my-0">{{ title }}</h3>
   </div>
 </template>
 
