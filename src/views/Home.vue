e19ebde92ee43cb184c870ead2510469cf101f51
NicolineRusbak <1081543@ucn.dk>
2022-12-13 08:37:59 +0100

    3ymp39c styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,4 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "../components/HelloWorld.vue";
 import FrontpageHeader from "../components/FrontpageHeader.vue";
 
 </script>
@@ -10,10 +7,27 @@ import FrontpageHeader from "../components/FrontpageHeader.vue";
   <FrontpageHeader />
 
   <section class="wrapper">
-    <h2>Kommende begivenheder</h2>
+    <div class="flex justify-content-between align-items-center ">
+      <h2>Kommende begivenheder</h2>
+      <div class="see-more-link">
+        <!-- send data med der fortæller hvilken sorteringen der skal være -->
+        <router-link :to="{ name: 'AllEvents' }">
+          <p>Se alle </p> <i class="pi pi-chevron-right"></i>
+        </router-link>
+      </div>
+    </div>
     <div class="upcoming-events-container"></div>
 
-    <h2>Mine begivenheder</h2>
+
+    <div class="flex justify-content-between align-items-center ">
+      <h2>Mine begivenheder</h2>
+      <div class="see-more-link">
+        <!-- send data med der fortæller hvilken sorteringen der skal være -->
+        <router-link :to="{ name: 'AllEvents' }">
+          <p>Se alle </p> <i class="pi pi-chevron-right"></i>
+        </router-link>
+      </div>
+    </div>
     <div class="my-events-container"></div>
 
     <h2>Grupper</h2>
@@ -32,5 +46,17 @@ import FrontpageHeader from "../components/FrontpageHeader.vue";
 }
 .logo:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.see-more-link {
+  a {
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+
+    p {
+      font-size: 12px;
+    }
+  }
 }
 </style>
