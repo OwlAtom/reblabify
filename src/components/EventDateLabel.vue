8a88e6150e00688dbd3f9684292917528e08c265
KamillaKlavsen <kamillaklavsen@outlook.dk>
2022-12-15 17:30:26 +0100

    3ymp3rg > feat: dato label styling

---
+++ src/components/EventDateLabel.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+  month: String,
+  date: Number,
+});
+</script>
+
+<template>
+  <div class="flex align-items-center justify-content-center flex-column div">
+    <h3 class="m-0">{{ month }}</h3>
+    <h1 class="m-0">{{ date }}</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  background-color: $secondary;
+  height: 89px;
+  width: 57px;
+  border-radius: 16px;
+  h3 {
+    font-size: 1rem;
+    font-weight: 700;
+  }
+  h1 {
+    font-weight: 700;
+  }
+}
+</style>
