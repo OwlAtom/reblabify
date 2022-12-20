69edb14f31189c1ee4e660c1b6f14d4ad2e809b7
NicolineRusbak <1081543@ucn.dk>
2022-12-20 08:29:13 +0100

    3x189g0 > chips styling

---
+++ src/components/DescriptionChips.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps({
+  text: String,
+  emoji: String,
+});
+</script>
+
+<template>
+  <div class="chip">
+    <p class="emoji">{{ emoji }}</p>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.chip {
+  display: flex;
+  align-items: center;
+  border-radius: 1em;
+  background-color: $white;
+  padding: 0.6em;
+  width: max-content;
+
+  p {
+    padding-right: 10px;
+    font-size: 12px;
+    margin: 0;
+
+    &.emoji {
+      font-size: 18px;
+    }
+  }
+}
+</style>
